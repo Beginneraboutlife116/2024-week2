59d5a6caa72921568d33b8252523e88417c243df
Beginneraboutlife116 <pythonforeye@gmail.com>
2024-07-27 01:32:23 +0800

    Update vite.config.js to export svg file test

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
 import "./assets/sass/main.scss";
-import upRightArrow from "./assets/images/up-right-arrow.svg";
-import upRightArrowWhite from "./assets/images/up-right-arrow-white.svg";
-
-document.querySelector(".btn__icon").src = upRightArrow;
-document.querySelector(".btn__icon--white").src = upRightArrowWhite;
--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "https://Beginneraboutlife116.github.io/2024-week2/",
+  build: {
+    assetsInlineLimit: 0,
+  },
 });
